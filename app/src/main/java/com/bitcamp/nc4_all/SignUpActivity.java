f62cb6594504e834bc19fba6260cba00833eba5a
Mooho Lee <dlangh6632@naver.com>
2024-03-29 19:13:27 +0900

    feat: 회원가입 화면 구현

---
+++ app/src/main/java/com/bitcamp/nc4_all/SignUpActivity.java
@@ -0,0 +1,25 @@
+package com.bitcamp.nc4_all;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bitcamp.nc4_all.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivitySignUpBinding binding = ActivitySignUpBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
+
+    }
+}
